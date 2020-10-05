b0406f06c832ab4659d300572567c4e7c84f0c83
ORBIS ALONZO GUTIERREZ <orbisalonzo@gmail.com>
2020-10-05 17:40:36 -0400

    adding Note and remove required field DNI patient v2

---
+++ HiDentalAPI/DataBaseLayer/Migrations/20201005214013_AddingNoteField.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace DataBaseLayer.Migrations
+{
+    public partial class AddingNoteField : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "IdentificationCard",
+                table: "Patients",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Note",
+                table: "Patients",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Note",
+                table: "Patients");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "IdentificationCard",
+                table: "Patients",
+                type: "nvarchar(max)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldNullable: true);
+        }
+    }
+}
